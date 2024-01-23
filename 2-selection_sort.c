fb89e49027d46f33c7612e642afb131cbb8544c2
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-01-23 09:16:10 -0800

    Sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * selection_sort - A function that sorts the array by selection.
+ * @array: Array to be sorted.
+ * @size: The size of the array.
+ * Return: Nothing.
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	int dan, n;
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		dan = array[i];
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < dan)
+			{
+				dan = array[j];
+				n = j;
+			}
+		}
+		array[n] = array[i];
+		array[i] = dan;
+		print_array(array, size);
+	}
+}
